e4389a88e2293ef3199264ee22086f0514c1b179
Raraa <70193585+Aisyaharani@users.noreply.github.com>
2023-04-06 14:31:50 +0700

    Add files via upload

---
+++ ExampleStack.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package strukdat3;
+import java.util.Stack;
+/**
+ *
+ * @author AISYAH
+ */
+public class ExampleStack {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Stack st = new Stack();
+        
+        st.push("Aku");
+        st.push("Anak");
+        st.push("Indonesia");
+        
+        System.out.println("Next :\n"+st.peek());
+        st.push("Kaya");
+        System.out.println(st.pop());
+        st.push("!");
+        
+        int count = st.search("Aku");
+        while (count != -1 && count>1){
+            st.pop();
+            count--;
+        }
+        System.out.println(st.pop());
+        System.out.println(st.empty());
+        
+    }
+    
+}
